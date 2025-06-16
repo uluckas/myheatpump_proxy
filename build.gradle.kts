36492d8572055ce0dbeeff4a99bb95dbf094376e
Uli Luckas <ulrich.luckas@accenture.com>
2025-06-16 17:49:24 +0200

    Switch to supported jdk distribution and switch to Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 kotlin {
     jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion.set(JavaLanguageVersion.of(17))
         vendor.set(JvmVendorSpec.AMAZON)
     }
 
