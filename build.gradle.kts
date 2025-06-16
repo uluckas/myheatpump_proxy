722f6375ece59aa1bc0df720ada40d71e4533a68
Uli Luckas <ulrich.luckas@accenture.com>
2025-06-16 10:13:54 +0200

    Fix build targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.9.22"
-    application
+    kotlin("multiplatform") version "2.1.20"
 }
 
 repositories {
@@ -8,21 +7,28 @@ repositories {
 }
 
 kotlin {
+    jvmToolchain(17)
+
     jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
+        @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
+        binaries {
+            // Configures a JavaExec task named "runJvm" and a Gradle distribution for the "main" compilation in this target
+            executable {
+                mainClass.set("foo.MainKt")
+            }
         }
         testRuns["test"].executionTask.configure {
             useJUnitPlatform()
         }
     }
-    linuxArm32Hfp("linuxArm32Hfp")
-    androidNativeArm32("androidNativeArm32") // Added this target
-
-    nodejs {
-        testTask {
-            useMocha {
-                timeout = "30s"
+    @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
+    wasmJs {
+        nodejs {
+            binaries.executable()
+            testTask {
+                useMocha {
+                    timeout = "30s"
+                }
             }
         }
     }
@@ -44,33 +50,7 @@ kotlin {
         val jvmMain by getting
         val jvmTest by getting
 
-        // Configuring all native targets to share a common nativeMain/nativeTest
-        val nativeMain by creating { // ensure this is 'creating' if it might not exist due to no native targets initially
-            dependsOn(commonMain)
-        }
-        val nativeTest by creating { // ensure this is 'creating'
-            dependsOn(commonTest)
-        }
-
-        // This loop will now apply to linuxArm32Hfp and androidNativeArm32
-        targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget>().forEach { target ->
-            val mainSourceSet = sourceSets.getByName("${target.name}Main")
-            mainSourceSet.dependsOn(nativeMain)
-
-            val testSourceSet = sourceSets.getByName("${target.name}Test")
-            testSourceSet.dependsOn(nativeTest)
-        }
-
-        val linuxArm32HfpMain by getting
-        val linuxArm32HfpTest by getting
-        val androidNativeArm32Main by getting // Added
-        val androidNativeArm32Test by getting // Added
-
-        val nodejsMain by getting
-        val nodejsTest by getting
+        val wasmJsMain by getting
+        val wasmJsTest by getting
     }
-}
-
-application {
-    mainClass.set("com.example.proxy.MainKt")
 }
